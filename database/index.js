e6e6a68fc5f8572fb3698e45aeabae7b2197bc26
EseAH <ese.aquino@gmail.com>
2023-10-05 15:52:00 -0300

    revision metodos schemas

--- database/index.js
+++ database/index.js
@@ -1,5 +1,12 @@
 const server = require('./src/server')
 
+const {Character} = require('./src/database')
+
+Character.list()
+.then(res=>console.log(res))
+Character.get(1)
+.then(res=>console.log(res))
+
 server.listen(8004, () => {
     console.log('Database service on PORT 8004');
 });
--- database/src/database/index.js
+++ database/src/database/index.js
@@ -6,10 +6,10 @@ const conn = mongoose.createConnection(MONGO_URI);
 // const Character = conn.model('Character', require('./schemas/characterSchema'));
 // const Film = conn.model('Film', require('./schemas/filmSchema'))
 
-Character.find().populate('films').then((res) => console.log(res));
+//Character.find().populate('films').then((res) => console.log(res));
 
 module.exports={
     Character: conn.model('Character', require('./schemas/characterSchema')),
     Film: conn.model('Film', require('./schemas/filmSchema')),
-    //Planet:
-}
+    Planet: conn.model('Planet', require('./schemas/planetSchema')),
+};
