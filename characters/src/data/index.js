b05b49b7ac6eff83366a5aec4cab87e0b94d1c4e
EseAH <ese.aquino@gmail.com>
2023-10-06 02:28:41 -0300

    fix bugs

--- characters/src/data/index.js
+++ characters/src/data/index.js
@@ -1,8 +1,10 @@
-const characters = require('./characters.json');
+//const characters = require('./characters.json');
 
 module.exports = {
     list: async () => {
-        return characters;
+        const res = await fetch("http://database:8004/Character")
+        const { data } = await res.json()
+        return data;
     },
     create: async () => {
         throw Error('Hay un error en BD al momento de crear el personaje')
