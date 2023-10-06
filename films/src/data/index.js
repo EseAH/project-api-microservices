d6395b92173dd703d6890a4ae2cdf4fcfc7ed8d8
EseAH <ese.aquino@gmail.com>
2023-10-06 03:02:05 -0300

    improved fn

--- films/src/data/index.js
+++ films/src/data/index.js
@@ -1,8 +1,10 @@
-const films = require('./films.json');
+//const films = require('./films.json');
+const axios = require('axios')
 
 module.exports = {
     list: async () => {
-        return films;
+        const res = await axios.get("http://database:8004/Film")
+        return res.data;
     },
     // create: async () => {
     //     throw Error('Hay un error en BD al momento de crear el film')
--- planets/src/data/index.js
+++ planets/src/data/index.js
@@ -1,8 +1,10 @@
-const planets = require('./planets.json');
+//const planets = require('./planets.json');
+const axios = require('axios')
 
 module.exports = {
     list: async () => {
-        return planets;
+        const res = await axios.get("http://database:8004/Planet")
+        return res.data;
     },
     // create: async () => {
     //     throw Error('Hay un error en BD al momento de crear el film')
